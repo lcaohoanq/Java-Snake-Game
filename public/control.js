492bf2e723bb097c7f2be98982129b05042cae4d
hoangclw@gmail.com <hoangclw@gmail.com>
2024-03-08 14:07:04 +0700

    Initial Commit

---
+++ public/control.js
@@ -0,0 +1,7 @@
+document.querySelector(".log").addEventListener("click", (e) => {
+    window.location.href = "/login.html";
+});
+
+document.querySelector(".reg").addEventListener("click", (e) => {
+    window.location.href = "/register.html";
+});
