60d86bfeb556a3e48223d1239c3a443cc9acba38
lcaohoanq <hoangclw@gmail.com>
2024-07-02 08:31:29 +0700

    feat: map container port

--- src/main/java/services/DatabaseConnection.java
+++ src/main/java/services/DatabaseConnection.java
@@ -12,9 +12,9 @@ public class DatabaseConnection {
 
     public Connection getConnection() throws SQLException {
         try {
-            String dbUrl = EnvUtils.get("DB_URL");
-            String dbUsername = EnvUtils.get("DB_USERNAME");
-            String dbPassword = EnvUtils.get("DB_PASSWORD");
+            String dbUrl = EnvUtils.get("DB_URL_DOCKER");
+            String dbUsername = EnvUtils.get("MYSQL_USER");
+            String dbPassword = EnvUtils.get("MYSQL_PASSWORD");
             return DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
         } catch (Exception e) {
             throw new DBException(ErrorMessages.ERROR_READ_FILE_ENV + e.getMessage());
