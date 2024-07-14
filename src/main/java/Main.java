cf69c2c527e1d8177f7a3784ede8c89c21231381
lcaohoanq <hoangclw@gmail.com>
2024-07-15 06:34:42 +0700

    feat: add create logs folder if not exist

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
 import java.awt.EventQueue;
 
+import utils.LogsUtils;
 import views.LoginView;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        //create logs folder if it does not exist
+        LogsUtils.ensureLogsFolderExists();
+
         EventQueue.invokeLater(() -> {
             new LoginView().setVisible(true);
         });
